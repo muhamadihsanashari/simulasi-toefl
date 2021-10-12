79919d33cb5311ee021f32485ed5bda471718785
rizky <nalandyarizky@gmail.com>
2021-10-12 11:30:46 +0700

    main menu

---
+++ app/src/main/java/com/fastwork/toefl/ui/splash/SplashScreen.kt
@@ -0,0 +1,38 @@
+package com.fastwork.toefl.ui.splash
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.fastwork.toefl.R
+import com.fastwork.toefl.databinding.FragmentSplashBinding
+import kotlinx.coroutines.*
+
+
+@DelicateCoroutinesApi
+class SplashScreen : Fragment() {
+
+    lateinit var binding: FragmentSplashBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_splash, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        GlobalScope.launch(Dispatchers.Main) {
+            delay(3000)
+            findNavController().navigate(R.id.action_loginFragment_to_mainFragment)
+        }
+    }
+}
